f3f7f84b38b6d734aabe5145ec09e6a1aa7bc8cc
Alex Browne <stephenalexbrowne@gmail.com>
2015-01-16 21:50:38 -0500

    Add larger test cases, but skip them for the recursive implementation.

--- test/pascal_test.go
+++ test/pascal_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"github.com/albrow/go-pascal/common"
 	"github.com/albrow/go-pascal/implementations"
+	"reflect"
 	"testing"
 )
 
@@ -34,10 +35,17 @@ func testPascaler(t *testing.T, p common.Pascaler) {
 		{7, 3, 35},
 		{7, 0, 1},
 		{7, 7, 1},
+		{32, 16, 601080390},
+		{64, 32, 1832624140942590534},
 	}
 
 	// Iterate through each test case and check the result
 	for _, c := range cases {
+		if reflect.TypeOf(p) == reflect.TypeOf(implementations.Recursive) && c.n > 30 {
+			// Skip cases where n is too large for the recursive implementation.
+			// It takes too long and might even timeout.
+			continue
+		}
 		got := p.Pascal(c.n, c.m)
 		if got != c.expected {
 			t.Errorf("Incorrect result for %s with inputs (%d, %d).\nExpected %d but got %d.", p, c.n, c.m, c.expected, got)
